56e951832c54f636824f121c993fbea98fef56d4
Petter Padilha <petterson_sorriso@hotmail.com>
2023-03-14 14:07:33 -0400

    Criando o conteudo da table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,44 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transaction() {
     return(
         <div>
             <Header/>
             <Summary/>
+
+            {/* Table */}
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                                <td>
+                                    <PriceHighLight variant="income">
+                                        R$ 12.000,00
+                                    </PriceHighLight>
+                                </td>
+                            <td>Vendas</td>
+                            <td>13/04/2022</td>
+                        </tr>
+
+                        {/* row */}
+
+                        <tr>
+                            <td width="50%">Lanche</td>
+                            <td>
+                                <PriceHighLight variant="outcome">
+                                    - R$ 59,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>10/04/2022</td>
+                        </tr>
+                        
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     );
 }
