b1dd16efb0097ff117f124f389100facb115d12d
Petter Padilha <petterson_sorriso@hotmail.com>
2023-03-14 22:37:22 -0400

    Finalizando modal de  cadastrar transações

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,19 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionsModal } from "../NewTransactionsModal";
 
 export function Header() {
     return(
         <HeaderContainer>
             <HeaderContent>
                 <h1>App <span>Money</span> </h1>
-                <NewTransactionButton>Nova Transação</NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova Transação</NewTransactionButton>
+                    </Dialog.Trigger>
+                    
+                    <NewTransactionsModal/>
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     );
