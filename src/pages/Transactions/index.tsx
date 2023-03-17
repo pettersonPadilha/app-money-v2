456e468535d4df418875b043c6824a760f6b70e0
Petter Padilha <petterson_sorriso@hotmail.com>
2023-03-17 11:39:27 -0400

    configurado json

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transaction() {
+
+    interface TransactionProps {
+        id:number;
+        description:string;
+        category: string;
+        price:number;
+        type: 'income' | 'outcome';
+        createdAt: string;
+    }
+
+    const [transaction , setTransaction] = useState<TransactionProps[]>([]);
+
+    async function loadTransactions() {
+        const response = await fetch("http://localhost:3333/transaction")
+        const data = await response.json();
+        setTransaction(data);
+    }
+    
+    useEffect(() => {
+        loadTransactions();
+    },[])
+
+
     return(
         <div>
             <Header/>
@@ -14,30 +38,20 @@ export function Transaction() {
                 <SearchForm/>
                 <TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                                <td>
-                                    <PriceHighLight variant="income">
-                                        R$ 12.000,00
-                                    </PriceHighLight>
-                                </td>
-                            <td>Vendas</td>
-                            <td>13/04/2022</td>
-                        </tr>
-
-                        {/* row */}
-
-                        <tr>
-                            <td width="50%">Lanche</td>
-                            <td>
-                                <PriceHighLight variant="outcome">
-                                    - R$ 59,00
-                                </PriceHighLight>
-                            </td>
-                            <td>Alimentação</td>
-                            <td>10/04/2022</td>
-                        </tr>
-
+                        {transaction.map(item => {
+                            return(
+                                <tr key={item.id}>
+                                <td width="50%">{item.description}</td>
+                                    <td>
+                                        <PriceHighLight variant={item.type}>
+                                           {item.price}
+                                        </PriceHighLight>
+                                    </td>
+                                <td>{item.category}</td>
+                                <td>{item.createdAt}</td>
+                            </tr>
+                            )
+                        })}
                     </tbody>
                 </TransactionsTable>
             </TransactionsContainer>
